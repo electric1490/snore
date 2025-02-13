68b56350f31863ac7293b6132062342a77b72e98
electric1490 <44627949+electric1490@users.noreply.github.com>
2025-02-12 22:01:18 -0500

    Update startup_script.sh

--- startup_script.sh
+++ startup_script.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#place this in the same folder as the executable
+#/home/snore/models/research/audioset/yamnet
+
 source snoreappENV/bin/activate
 
 python models/research/audioset/yamnet/yamnet_realtime_engine.py
+
